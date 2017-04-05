447a2545474f1780b527dccf70de3e4ad726cc10
Chris Benco <chrisbenco@gmail.com>
2017-04-05 10:07:13 +1000

    changed unit testing framework from MSUnit to XUnit

---
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace GoGoPowerRangers.ENET.Tests
+{
+	public class Test
+	{
+		[Fact ()]
+		public void TestMethod ()
+		{
+			Assert.True (false, "this is a test");
+		}
+	}
+}
+
