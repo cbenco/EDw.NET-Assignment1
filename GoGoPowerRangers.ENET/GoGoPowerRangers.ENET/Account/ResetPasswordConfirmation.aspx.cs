8d31ca4e64d9b4b402a21be7e6be0ff5a2b28e09
Chris Benco <Chris Benco>
2017-04-05 17:32:35 +1000

    rebuilt project to solve mscorlib issues

--- GoGoPowerRangers.ENET/GoGoPowerRangers.ENET/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace GoGoPowerRangers.ENET.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- GoGoPowerRangers.ENET/GoGoPowerRangers.ENET/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoGoPowerRangers.ENET.Global" Language="C#" %>
