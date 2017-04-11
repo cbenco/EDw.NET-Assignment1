8815c54634231a8f78669e2db17556e30569be75
luke <lgvsingle@gmail.com>
2017-04-11 22:53:31 +0900

    Added asp project & git thinking benco's changes to Tests.cs are new idk sorry for merge conflicts

---
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Model/Manager_BACKUP_10264.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoGoPowerRangers.ENET.Model
+{
+    public class Manager : User
+    {
+        public List<Intervention> _interventions = new List<Intervention>();
+        
+        public Manager(User user) : base(user)
+        {
+            District = new District();
+            for (int i = 0; i < 12; i++)
+            {
+                Intervention intervention = new Intervention();
+                switch (i % 4)
+                {
+                    case 0:
+                        intervention.Status = Status.Approved;
+                        break;
+                    case 1:
+                        intervention.Status = Status.Pending;
+                        break;
+                    case 2:
+                        intervention.Status = Status.Complete;
+                        break;
+                    case 3:
+                        intervention.Status = Status.Cancelled;
+                        break;
+                }      
+                    
+                _interventions.Add(intervention);
+            }
+        }
+        public District District { get; set; }
+
+        //possibly need to change this to a public property 
+        //to bind to datalist in winforms
+        public List<Intervention> GetPendingInterventions()
+        {
+            List<Intervention> interventions = new List<Intervention>();
+            interventions.Where(s => s.Status == Status.Pending);
+
+            return interventions;
+        }
+        public void ChangeSiteEngineerDistrict(SiteEngineer engineer, District district)
+        {
+<<<<<<< Updated upstream
+            throw new NotImplementedException();
+            //engineer.District = district;
+=======
+            engineer.District = district;
+>>>>>>> Stashed changes
+        }
+        public void ChangeManagerDistrict(Manager manager, District district)
+        {
+            manager.District = district;
+        }
+    }
+}
---
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Model/Manager_BASE_10264.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoGoPowerRangers.ENET.Model
+{
+    public class Manager : User
+    {
+        public Manager(User user) : base(user) { }
+        public District District { get; set; }
+
+        public List<Intervention> GetProposedInterventions()
+        {
+            List<Intervention> interventions = new List<Intervention>();
+            //replace above with 
+            //var Intervention = linq query
+            return interventions;
+        }
+        public void ChangeSiteEngineerDistrict(SiteEngineer engineer, District district)
+        {
+            //engineer.District = district;
+        }
+        public void ChangeManagerDistrict(Manager manager, District district)
+        {
+            //manager.District = district;
+        }
+
+
+    }
+}
