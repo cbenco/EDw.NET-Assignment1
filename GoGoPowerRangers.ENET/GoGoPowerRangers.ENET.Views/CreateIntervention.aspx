8f8f28e45d4ddf5e677be3ebbdc5a613f582780b
Chris Benco <chrisbenco@gmail.com>
2017-04-19 19:11:05 +1000

    still trying to fix it

---
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Views/CreateIntervention.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateIntervention.aspx.cs" Inherits="GoGoPowerRangers.ENET.Views.CreateIntervention" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Create Intervention</h1>
+        </div>
+        <div>
+            <asp:DropDownList ID="types" runat="server" OnSelectedIndexChanged="types_SelectedIndexChanged" AppendDataBoundItems="true">
+            </asp:DropDownList>
+            type<br />
+            <asp:DropDownList ID="clients" runat="server">
+            </asp:DropDownList>
+            client<br />
+            <asp:TextBox ID="manHours" runat="server" EnableViewState="False"></asp:TextBox>
+            man-hours<br />
+            <asp:TextBox ID="materialCost" runat="server"></asp:TextBox>
+            material cost<br />
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server" Height="118px" Width="242px"></asp:TextBox>
+            notes</div>
+    </form>
+</body>
+</html>
---
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Views/CreateIntervention.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GoGoPowerRangers.ENET.Data;
+using GoGoPowerRangers.ENET.Model;
+
+namespace GoGoPowerRangers.ENET.Views
+{
+    public partial class CreateIntervention : System.Web.UI.Page
+    {
+        SiteEngineer _user;
+        FakeDatabase fakeDb = new FakeDatabase();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //_user = (SiteEngineer)Session["currentUser"];
+                //FakeDatabase fakeDb = new FakeDatabase();
+
+                _user = (SiteEngineer)fakeDb._users[0];
+                types.DataSource = fakeDb._interventionTypes;
+                types.DataTextField = "Name";
+                types.DataValueField = "Id";
+                types.DataBind();
+
+                clients.DataSource = _user.ListClientsInDistrict();
+                clients.DataTextField = "Name";
+                clients.DataValueField = "Id";
+                clients.DataBind();
+
+                manHours.Text = fakeDb._interventionTypes[types.SelectedIndex].ManHours.ToString();
+                materialCost.Text = fakeDb._interventionTypes[types.SelectedIndex].MaterialCost.ToString();
+            }
+        }
+
+        protected void types_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                manHours.Text = fakeDb._interventionTypes[types.SelectedIndex].ManHours.ToString();
+                materialCost.Text = fakeDb._interventionTypes[types.SelectedIndex].MaterialCost.ToString();
+        }
+    }
+}
