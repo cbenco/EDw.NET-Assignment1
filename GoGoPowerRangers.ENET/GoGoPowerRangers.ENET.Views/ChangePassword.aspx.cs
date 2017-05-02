d13c7a3202ca4eff889c3bc5fb82a875365db5df
lukesingle <luke.single@student.uts.edu.au>
2017-05-02 22:52:41 +1000

    Change password validation

--- GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Views/ChangePassword.aspx.cs
+++ GoGoPowerRangers.ENET/GoGoPowerRangers.ENET.Views/ChangePassword.aspx.cs
@@ -35,8 +35,43 @@ namespace GoGoPowerRangers.ENET.Views
         protected void changePasswordButton_Click(object sender, EventArgs e)
         {           
             var password = currentPassword.Text;
-            UserTableAdapter usersTable = new UserTableAdapter();
-            usersTable.UpdateUserPassword(newPassword.Text, _user.UserName, currentPassword.Text, _user.Id);
+
+            if (password != _user.Password)
+                ErrorMessage.Text = "Incorrect password";
+            else if (password == newPassword.Text)
+                ErrorMessage.Text = "New Password must be different";
+            else if (newPassword.Text != confirmPassword.Text)
+                ErrorMessage.Text = "New passwords must match";
+            else if (password.Count() == 0)
+                ErrorMessage.Text = "Password must contain characters";
+            else
+            {
+                try
+                {
+                    UserTableAdapter usersTable = new UserTableAdapter();
+                    usersTable.UpdateUserPassword(newPassword.Text, _user.UserName, currentPassword.Text, _user.Id);
+                    ErrorMessage.Text = "Password Changed!";
+
+                    _user.Password = newPassword.Text;
+                    Session["currentUser"] = _user;
+
+                    if (_user.GetType().Equals(typeof(Accountant)))
+                        Response.Redirect("Accountant.aspx", false);
+
+                    if (_user.GetType().Equals(typeof(Manager)))
+                        Response.Redirect("Manager.aspx", false);
+
+                    if (_user.GetType().Equals(typeof(SiteEngineer)))
+                        Response.Redirect("Engineer.aspx", false);
+                }
+                catch
+                {
+                    Response.Redirect("LoginPage.aspx");
+                }
+            }
+           
+
+
         }
     }
 }
